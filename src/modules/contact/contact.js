7e0088b3ab8bb15deeacfb49fe96efb9487b00e9
omni23 <89713821+omni23@users.noreply.github.com>
2023-05-15 17:51:17 -0400

    Add contact page functions and styling

---
+++ src/modules/contact/contact.js
@@ -0,0 +1,79 @@
+import createHTMLElement from "../helpers/helpers";
+import "./contact.css";
+
+function createContact() {
+  const main = document.querySelector("main");
+
+  const heading = createHTMLElement("h2", "", "Contact Our Staff");
+
+  const fnameLabel = createHTMLElement(
+    "label",
+    { for: "fname" },
+    "First Name: "
+  );
+  const fnameInput = createHTMLElement("input", { type: "text", id: "fname" });
+  const fnameDiv = createHTMLElement(
+    "div",
+    { class: "form-div" },
+    fnameLabel,
+    fnameInput
+  );
+
+  const lnameLabel = createHTMLElement(
+    "label",
+    { for: "lname" },
+    "Last Name: "
+  );
+  const lnameInput = createHTMLElement("input", { type: "text", id: "lname" });
+  const lnameDiv = createHTMLElement(
+    "div",
+    { class: "form-div" },
+    lnameLabel,
+    lnameInput
+  );
+
+  const emailLabel = createHTMLElement("label", { for: "email" }, "Email: ");
+  const emailInput = createHTMLElement("input", { type: "email", id: "email" });
+  const emailDiv = createHTMLElement(
+    "div",
+    { class: "form-div" },
+    emailLabel,
+    emailInput
+  );
+
+  const messageLabel = createHTMLElement(
+    "label",
+    { for: "message" },
+    "Message: "
+  );
+  const messageInput = createHTMLElement("textarea", {
+    id: "message",
+    rows: 10,
+    cols: 40,
+  });
+  const messageDiv = createHTMLElement(
+    "div",
+    { class: "form-div" },
+    messageLabel,
+    messageInput
+  );
+
+  const formButton = createHTMLElement("button", "", "Submit");
+
+  const form = createHTMLElement("form");
+
+  form.append(fnameDiv, lnameDiv, emailDiv, messageDiv, formButton);
+
+  const contactSection = createHTMLElement(
+    "section",
+    {
+      id: "contact-section",
+    },
+    heading,
+    form
+  );
+
+  main.append(contactSection);
+}
+
+export default createContact;
