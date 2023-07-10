72353d35ecdf5682f696ef1a06721f84eee8d93e
omni23 <89713821+omni23@users.noreply.github.com>
2023-07-10 12:15:12 -0400

    commit config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
